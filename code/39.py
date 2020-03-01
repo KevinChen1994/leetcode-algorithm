bbfee990a9c4cb6e2e875fd40673aaecdfdfda6c
kevinchen1994 <346521888@qq.com>
2020-03-01 22:32:21 +0800

    39

---
+++ code/39.py
@@ -0,0 +1,48 @@
+# -*- coding:utf-8 -*-
+# author:chenmeng
+# datetime:2020/3/1 21:42
+# software: PyCharm
+
+'''
+深度优先搜索
+'''
+
+
+class Solution:
+    def combinationSum(self, candidates, target):
+        size = len(candidates)
+        if size == 0:
+            return []
+        # 剪枝是为了提速，在本题非必需
+        candidates.sort()
+        # 在遍历的过程中记录路径，它是一个栈
+        path = []
+        res = []
+        # 注意要传入 size ，在 range 中， size 取不到
+        self.__dfs(candidates, 0, size, path, res, target)
+        return res
+
+    def __dfs(self, candidates, begin, size, path, res, target):
+        # 先写递归终止的情况
+        if target == 0:
+            # Python 中可变对象是引用传递，因此需要将当前 path 里的值拷贝出来
+            # 或者使用 path.copy()
+            res.append(path[:])
+            return
+
+        for index in range(begin, size):
+            residue = target - candidates[index]
+            # “剪枝”操作，不必递归到下一层，并且后面的分支也不必执行
+            if residue < 0:
+                break
+            path.append(candidates[index])
+            # 因为下一层不能比上一层还小，起始索引还从 index 开始
+            self.__dfs(candidates, index, size, path, res, residue)
+            path.pop()
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    candidates = [2, 3, 6, 7]
+    target = 7
+    print(solution.combinationSum(candidates, target))

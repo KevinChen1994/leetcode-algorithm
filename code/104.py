318cdae94edbe6fcf172d98c3c5f706a2d039427
kevinchen1994 <346521888@qq.com>
2020-03-27 22:00:25 +0800

    update

--- code/104.py
+++ code/104.py
@@ -2,7 +2,9 @@
 # author:chenmeng
 # datetime:2020/3/26 23:27
 # software: PyCharm
-
+'''
+solution: 两种深度优先遍历的实现方法
+'''
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
@@ -21,4 +23,16 @@ class Solution:
             right_ = self.maxDepth(root.right)
             return max(left_, right_) + 1
 
-
+    # 迭代
+    def maxDepth_2(self, root):
+        stack = []
+        if root:
+            stack.append((1, root))
+        depth = 0
+        while stack:
+            cur_depth, node = stack.pop()
+            if node:
+                depth = max(cur_depth, depth)
+                stack.append((cur_depth + 1, node.left))
+                stack.append((cur_depth + 1, node.right))
+        return depth
